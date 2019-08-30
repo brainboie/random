c1f782a706d399b6d66dec93e83caaf9785a8035
brainboie <54725275+brainboie@users.noreply.github.com>
2019-08-30 16:39:14 -0400

    Add files via upload

--- Write-PSBCAnswer.ps1
+++ Write-PSBCAnswer.ps1
@@ -1,2 +1,2 @@
 ﻿# PowerShell Brain Candy
-$j = 0; foreach($i in (1..9)) { $j = $j*10+$i; write-host "$j x 8 + $i = $($j*8+$i)" }
+$j = 0; foreach($i in (1..9)) { $j = $j*10+$i; write-host "$(" "*(9-$i))$j x 8 + $i = $($j*8+$i)" }
